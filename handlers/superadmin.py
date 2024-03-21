0bbe90cb5752054ffb12357772d2c51905a276ee
taus232323 <taus232323@gmail.com>
2024-03-21 08:59:07 +0300

    Обезопасил вывод списка пользователей

--- handlers/superadmin.py
+++ handlers/superadmin.py
@@ -18,7 +18,19 @@ router = Router()
 async def add_admin_chosed(message: Message, state: FSMContext):
     users = await get_users()
     user_info = "\n".join([f"id:{user.id} @{user.username}" for user in users])
-    await message.answer(user_info)
+    parts = []
+    while len(user_info) > 0:
+        if len(user_info) <= 4000:
+            parts.append(user_info)
+            user_info = ""
+        else:
+            cut_off = user_info.rfind("\n\n", 0, 4000)
+            if cut_off == -1:
+                cut_off = 4000 - user_info[0:4001][::-1].find("\n")
+            parts.append(user_info[:cut_off])
+            user_info = user_info[cut_off:].strip()
+    for part in parts:
+        await message.answer(part)
     await state.set_state(AddAdmin.admin_id)
     await message.answer('Введите ID пользователя которого хотите сделать администратором')
 
